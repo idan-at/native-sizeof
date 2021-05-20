680c0540401b166e9bb29d178551b484713c81d8
Idan Attias <idana@wix.com>
2021-05-20 10:18:31 +0300

    handle circular objects (inefficiently) and fmt

--- __tests__/integration_tests.spec.js
+++ __tests__/integration_tests.spec.js
@@ -1,66 +1,66 @@
-const {sizeof} = require("..");
+const { sizeof } = require("..");
 const chance = require("chance")();
 
 describe("native-sizeof", () => {
   describe("primitives", () => {
-      test("returns 0 when called without an argument", () => {
-        expect(sizeof()).toStrictEqual(0);
-      });
+    test("returns 0 when called without an argument", () => {
+      expect(sizeof()).toStrictEqual(0);
+    });
 
     // TODO:
     // - NaN
     // - Inf
     // - Symbols
-      test.each`
-      value | expected | description
-      ${undefined} | ${0} | ${"undefined"}
-      ${null} | ${0} | ${"null"}
-      ${function () {}} | ${0} | ${"a function"}
-      ${{}} | ${0} | ${"an empty object"}
-      ${chance.bool()} | ${4} | ${"a boolean"}
-      ${chance.integer()} | ${8} | ${"an integer number"}
-      ${chance.floating()} | ${8} | ${"an float number"}
+    test.each`
+      value                           | expected | description
+      ${undefined}                    | ${0}     | ${"undefined"}
+      ${null}                         | ${0}     | ${"null"}
+      ${function () {}}               | ${0}     | ${"a function"}
+      ${{}}                           | ${0}     | ${"an empty object"}
+      ${chance.bool()}                | ${4}     | ${"a boolean"}
+      ${chance.integer()}             | ${8}     | ${"an integer number"}
+      ${chance.floating()}            | ${8}     | ${"an float number"}
       ${chance.string({ length: 5 })} | ${5 * 2} | ${"a string of size 5"}
-      ${Buffer.alloc(5)} | ${5} | ${"a buffer of size 5"}
-      `("returns $expected for $description", ({ value, expected }) => {
-        expect(sizeof(value)).toStrictEqual(expected);
-      })
-  })
+      ${Buffer.alloc(5)}              | ${5}     | ${"a buffer of size 5"}
+    `("returns $expected for $description", ({ value, expected }) => {
+      expect(sizeof(value)).toStrictEqual(expected);
+    });
+  });
 
   describe("arrays", () => {
     test.each`
-    array | expected | description
-    ${[chance.integer(), chance.floating(), chance.integer()]} | ${8 * 3} | ${"an array with elements of the same type (3 numbers)"}
-    ${[chance.integer(), chance.string({ length: 3 })]} | ${8  + 3 * 2} | ${"an array with elements of the different types (1 number and 1 string of length 3)"}
-    ${[null, undefined, {}, chance.integer()]} | ${8} | ${"an array with some non counted elements (null, undefined, empty object and a number)"}
-    `("returns $expected for $description", ({array, expected}) => {
+      array                                                      | expected     | description
+      ${[chance.integer(), chance.floating(), chance.integer()]} | ${8 * 3}     | ${"an array with elements of the same type (3 numbers)"}
+      ${[chance.integer(), chance.string({ length: 3 })]}        | ${8 + 3 * 2} | ${"an array with elements of the different types (1 number and 1 string of length 3)"}
+      ${[null, undefined, {}, chance.integer()]}                 | ${8}         | ${"an array with some non counted elements (null, undefined, empty object and a number)"}
+    `("returns $expected for $description", ({ array, expected }) => {
       expect(sizeof(array)).toStrictEqual(expected);
-    })
-  })
+    });
+  });
 
   describe("objects", () => {
     test.each`
-    object | expected | description
-    ${{[chance.string({ length: 3 })]: chance.string({ length: 5 })}} | ${3 * 2 + 5 * 2} | ${"a simple object (string key of size 3 and a string value of size 5)"}
-    ${{[chance.string({ length: 5})]: [chance.string({ length: 3 })]}} | ${5 * 2 + 3 * 2} | ${"an object with an array value (string key of size 5 and array value with a string of size 3)"}
-    ${{[chance.integer({ min: 100, max: 999 })]: {[chance.string({ length: 3 })]: chance.string({length: 5})}}} | ${3 * 2 + 3 * 2 + 5 * 2} | ${"an object with nested objects (number key which is casted into a string, nested string key of size 3 and a nested value of size 5)"}
-    `("returns $expected for $description", ({object, expected}) => {
+      object                                                                                                            | expected                 | description
+      ${{ [chance.string({ length: 3 })]: chance.string({ length: 5 }) }}                                               | ${3 * 2 + 5 * 2}         | ${"a simple object (string key of size 3 and a string value of size 5)"}
+      ${{ [chance.string({ length: 5 })]: [chance.string({ length: 3 })] }}                                             | ${5 * 2 + 3 * 2}         | ${"an object with an array value (string key of size 5 and array value with a string of size 3)"}
+      ${{ [chance.integer({ min: 100, max: 999 })]: { [chance.string({ length: 3 })]: chance.string({ length: 5 }) } }} | ${3 * 2 + 3 * 2 + 5 * 2} | ${"an object with nested objects (number key which is casted into a string, nested string key of size 3 and a nested value of size 5)"}
+    `("returns $expected for $description", ({ object, expected }) => {
       expect(sizeof(object)).toStrictEqual(expected);
-    })
+    });
 
-    test.skip("ignores already seen objects in case of a cycle", () => {
-      const object = {abc: 123};
+    test("ignores already seen objects in case of a cycle", () => {
+      const object = { abc: 123 };
       object.cycle = object;
 
       expect(sizeof(object)).toStrictEqual(3 * 2 + 8 + 5 * 2);
-    })
+    });
 
     test("larger objects", () => {
       const object = {};
       const count = 100;
 
       for (let i = 0; i < count; i++) {
-        const key = chance.string({ length: 10 })
+        const key = chance.string({ length: 10 });
 
         object[key] = chance.bool();
       }
@@ -69,6 +69,6 @@ describe("native-sizeof", () => {
       const valuesSize = count * 4;
 
       expect(sizeof(object)).toStrictEqual(keysSize + valuesSize);
-    })
-  })
-})
+    });
+  });
+});
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
-use std::collections::HashSet;
 use neon::prelude::*;
-use neon::handle::Managed;
-use neon::macro_internal::runtime::raw;
 
 enum Sizes {
     Character = 2,
@@ -9,52 +6,69 @@ enum Sizes {
     Number = 8,
 }
 
-fn sizeof_value(cx: &mut FunctionContext, handle: Handle<JsValue>, seen: &mut HashSet<raw::Local>) -> u32 {
+fn sizeof_value<'a>(
+    cx: &mut FunctionContext<'a>,
+    handle: Handle<'a, JsValue>,
+    seen: &mut Vec<Handle<'a, JsValue>>,
+) -> u32 {
     if handle.is_a::<JsBoolean, _>(cx) {
         Sizes::Boolean as u32
     } else if handle.is_a::<JsNumber, _>(cx) {
         Sizes::Number as u32
     } else if handle.is_a::<JsString, _>(cx) {
-        handle.downcast_or_throw::<JsString, _>(cx).unwrap().size(cx) as u32 * Sizes::Character as u32
+        handle
+            .downcast_or_throw::<JsString, _>(cx)
+            .unwrap()
+            .size(cx) as u32
+            * Sizes::Character as u32
     } else if handle.is_a::<JsBuffer, _>(cx) {
         let buffer = handle.downcast_or_throw::<JsBuffer, _>(cx).unwrap();
 
         cx.borrow(&buffer, |buf| buf.as_slice::<u8>().len()) as u32
     } else if handle.is_a::<JsArray, _>(cx) {
-        let vec = handle.downcast_or_throw::<JsArray, _>(cx).unwrap().to_vec(cx).unwrap();
+        let vec = handle
+            .downcast_or_throw::<JsArray, _>(cx)
+            .unwrap()
+            .to_vec(cx)
+            .unwrap();
 
-        vec.into_iter().fold(0, |acc, value| acc + sizeof_value(cx, value, seen))
+        vec.into_iter()
+            .fold(0, |acc, value| acc + sizeof_value(cx, value, seen))
     } else if handle.is_a::<JsObject, _>(cx) {
-        // seen.insert(handle.to_raw());
+        seen.push(handle.clone());
 
         let object = handle.downcast_or_throw::<JsObject, _>(cx).unwrap();
 
-        let keys_handles = object.get_own_property_names(cx).unwrap().to_vec(cx).unwrap();
+        let keys_handles = object
+            .get_own_property_names(cx)
+            .unwrap()
+            .to_vec(cx)
+            .unwrap();
 
         keys_handles.into_iter().fold(0, |acc, key_handle| {
             let value_handle = object.get(cx, key_handle).unwrap();
 
-            // println!("{}", seen.len());
-
-            // if seen.contains(&value_handle.to_raw()) {
-            //     return acc;
-            // }
-
             let key_size = sizeof_value(cx, key_handle, seen);
+
+            if seen.iter().any(|v| value_handle.strict_equals(cx, *v)) {
+                return acc + key_size;
+            }
+
             let value_size = sizeof_value(cx, value_handle, seen);
 
             acc + key_size + value_size
         })
-    }
-    else {
+    } else {
         0
     }
 }
 
 fn sizeof(mut cx: FunctionContext) -> JsResult<JsNumber> {
-    let argument = cx.argument_opt(0).unwrap_or(cx.undefined().as_value(&mut cx));
+    let argument = cx
+        .argument_opt(0)
+        .unwrap_or(cx.undefined().as_value(&mut cx));
 
-    let mut seen: HashSet<raw::Local> = HashSet::new();
+    let mut seen: Vec<Handle<JsValue>> = Vec::new();
     let size = sizeof_value(&mut cx, argument, &mut seen);
 
     Ok(cx.number(size))
